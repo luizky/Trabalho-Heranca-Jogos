181efba2222546516a2641ca511f716e6735e052
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:30:46 -0300

    Create Acessorios.java

---
+++ Acessorios.java
@@ -0,0 +1,44 @@
+package br.unipar.jogo;
+
+public class Acessorios {
+
+    private String toca;
+    private String calca;
+    private String chapeu;
+
+    public String getToca() {
+        return toca;
+    }
+
+    public void setToca(String toca) {
+        this.toca = toca;
+    }
+
+    public String getCamisa() {
+        return camisa;
+    }
+
+    public void setCamisa(String camisa) {
+        this.camisa = camisa;
+    }
+
+    public String getCalca() {
+        return calca;
+    }
+
+    public void setCalca(String calca) {
+        this.calca = calca;
+    }
+
+    public String getChapeu() {
+        return chapeu;
+    }
+
+    public void setChapeu(String chapeu) {
+        this.chapeu = chapeu;
+    }
+
+    private String camisa;
+
+
+}
