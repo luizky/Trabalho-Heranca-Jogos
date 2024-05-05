89579ffa10d0fe81b8795ab6323f31c870ce063d
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:31:12 -0300

    Create Cores.java

---
+++ Cores.java
@@ -0,0 +1,13 @@
+package br.unipar.jogo;
+
+public class Cores {
+private String rgb;
+
+    public String getRgb() {
+        return rgb;
+    }
+
+    public void setRgb(String rgb) {
+        this.rgb = rgb;
+    }
+}
