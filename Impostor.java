fd259e47559a37c9f1a53072ebdb6346bb4838df
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:29:08 -0300

    Create Impostor.java

---
+++ Impostor.java
@@ -0,0 +1,36 @@
+package br.unipar.jogo;
+
+public class Impostor {
+    private Cores cores;
+    private Movimento movimento;
+    private Sabotagem sabotagem;
+    private Acessorios acessorios;
+    private Procedimentos procedimentos;
+
+    public Cores getCores() {
+        return cores;
+    }
+
+    public void setCores(Cores cores) {
+        this.cores = cores;
+    }
+
+    public Movimento getMovimento() {
+        return movimento;
+    }
+
+    public void setMovimento(Movimento movimento) {
+        this.movimento = movimento;
+    }
+
+    public Sabotagem getSabotagem() {
+        return sabotagem;
+    }
+
+    public void setSabotagem(Sabotagem sabotagem) {
+        this.sabotagem = sabotagem;
+    }
+
+    public Acessorios getAcessorios() {
+        return acessorios;
+    }
