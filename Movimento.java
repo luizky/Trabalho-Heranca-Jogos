c236d7d1257c071818b59a1338222eccf37c69d4
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:31:44 -0300

    Create Movimento.java

---
+++ Movimento.java
@@ -0,0 +1,42 @@
+package br.unipar.jogo;
+
+public class Movimento {
+    private Void cima;
+    private Void baixo;
+    private Void esquerda;
+    private Void direita;
+
+
+    public Void getCima() {
+        return cima;
+    }
+
+    public void setCima(Void cima) {
+        this.cima = cima;
+    }
+
+    public Void getBaixo() {
+        return baixo;
+    }
+
+    public void setBaixo(Void baixo) {
+        this.baixo = baixo;
+    }
+
+    public Void getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(Void esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public Void getDireita() {
+        return direita;
+    }
+
+    public void setDireita(Void direita) {
+        this.direita = direita;
+    }
+
+}
