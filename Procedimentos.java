260b6eeac642c130fa4b5af18bf36af6f498e95d
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:32:20 -0300

    Create Procedimentos.java

---
+++ Procedimentos.java
@@ -0,0 +1,33 @@
+package br.unipar.jogo;
+
+public class Procedimentos {
+    private String apertar;
+    private String usar;
+    private String reportar;
+    public String getApertar() {
+        return apertar;
+    }
+
+    public void setApertar(String apertar) {
+        this.apertar = apertar;
+    }
+
+    public String getUsar() {
+        return usar;
+    }
+
+    public void setUsar(String usar) {
+        this.usar = usar;
+    }
+
+    public String getReportar() {
+        return reportar;
+    }
+
+    public void setReportar(String reportar) {
+        this.reportar = reportar;
+    }
+
+
+
+}
