bf326c0aea87842c9be3a890f6af4eb2b3fd480e
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:30:07 -0300

    Create Desafios.java

---
+++ Desafios.java
@@ -0,0 +1,69 @@
+package br.unipar.jogo;
+
+public class Desafios {
+    private String lixo;
+    private String abastecer;
+    private String meteoro;
+    private String energia;
+    private String alinhamento;
+    private String cartao;
+    private String baixar;
+
+    public String getLixo() {
+        return lixo;
+    }
+
+    public void setLixo(String lixo) {
+        this.lixo = lixo;
+    }
+
+    public String getAbastecer() {
+        return abastecer;
+    }
+
+    public void setAbastecer(String abastecer) {
+        this.abastecer = abastecer;
+    }
+
+    public String getMeteoro() {
+        return meteoro;
+    }
+
+    public void setMeteoro(String meteoro) {
+        this.meteoro = meteoro;
+    }
+
+    public String getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(String energia) {
+        this.energia = energia;
+    }
+
+    public String getAlinhamento() {
+        return alinhamento;
+    }
+
+    public void setAlinhamento(String alinhamento) {
+        this.alinhamento = alinhamento;
+    }
+
+    public String getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(String cartao) {
+        this.cartao = cartao;
+    }
+
+    public String getBaixar() {
+        return baixar;
+    }
+
+    public void setBaixar(String baixar) {
+        this.baixar = baixar;
+    }
+
+    
+}
