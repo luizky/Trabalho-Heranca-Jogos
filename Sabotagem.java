17314d918a98809df7c14cd807e3bf9c8fb1fd85
luizky <160685655+luizky@users.noreply.github.com>
2024-05-05 17:32:57 -0300

    Create Sabotagem.java

---
+++ Sabotagem.java
@@ -0,0 +1,24 @@
+package br.unipar.jogo;
+
+public class Sabotagem {
+    private String sabotar;
+
+    public String getSabotar() {
+        return sabotar;
+    }
+
+    public void setSabotar(String sabotar) {
+        this.sabotar = sabotar;
+    }
+
+    public String getMatar() {
+        return matar;
+    }
+
+    public void setMatar(String matar) {
+        this.matar = matar;
+    }
+
+    private String matar;
+
+}
